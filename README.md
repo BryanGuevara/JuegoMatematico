5943a2c97048af8fe8cc10a2d79f6f01f4de5887
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2024-08-09 17:09:57 -0600

    Codigo

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# JuegoMatematico
+# Juego Matemático
+
+## Descripción
+
+**Juego Matemático** es una aplicación de consola en Java diseñada para poner a prueba tus habilidades matemáticas a través de una serie de problemas matemáticos simples. En cada ronda, el programa genera una operación aleatoria (suma, resta, multiplicación o división) y solicita al usuario que proporcione la respuesta. Al final del juego, se muestra el puntaje total basado en las respuestas correctas.
+
+## Funcionalidades
+
+- Generación aleatoria de operaciones matemáticas (suma, resta, multiplicación y división).
+- Evaluación de respuestas del usuario y cálculo del puntaje.
+- Manejo de errores para entradas no numéricas.
+- Formateo de respuestas y resultados para una presentación clara.
+
+## Uso
+
+1. Al ejecutar el programa, se te presentarán una serie de problemas matemáticos en 5 rondas.
+2. Ingresa tu respuesta para cada problema.
+3. Al final del juego, el programa mostrará tus respuestas, indicará cuáles fueron correctas o incorrectas y calculará tu puntaje final.
+
+## Ejemplo de Ejecución
+
+```plaintext
+=================================================== <[Juego]> ===================================================
+Ronda 1: 45+32=77
+Ronda 2: 89-21=68
+Ronda 3: 7X8=56
+Ronda 4: 100/4=25
+Ronda 5: 18+9=27
+=================================================== <[Resultados]> ===============================================
+Ronda 1: 77 es Correcto
+Ronda 2: 68 es Correcto
+Ronda 3: 56 es Correcto
+Ronda 4: 25 es Correcto
+Ronda 5: 27 es Incorrecto, el resultado era: 27
+Puntaje Final: 8
+==================================================================================================================
--- src/juego/JuegoMatematico.java
+++ src/juego/JuegoMatematico.java
@@ -1,20 +1,73 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package juego;
 
-/**
- *
- * @author MINEDUCYT
- */
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class JuegoMatematico {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            Random ran = new Random();
+            Scanner scan = new Scanner(System.in);
+            int ronda = 0;
+            String respuestas = "";
+            int puntos = 0;
+
+            while (ronda <= 4) {
+                int num1 = ran.nextInt(100) + 1;
+                int num2 = ran.nextInt(100) + 1;
+                float result = 0;
+                float resp = 0;
+                int ope = ran.nextInt(4);
+
+                System.out.println("=================================================== <[Juego]> ===================================================");
+                System.out.print("Ronda " + (ronda + 1) + ": ");
+                if (ope == 0) {
+                    System.out.print(num1 + "+" + num2 + "=");
+                    result = num1 + num2;
+                } else if (ope == 1) {
+                    System.out.print(num1 + "-" + num2 + "=");
+                    result = num1 - num2;
+                } else if (ope == 2) {
+                    System.out.print(num1 + "X" + num2 + "=");
+                    result = num1 * num2;
+                } else if (ope == 3) {
+                    System.out.print(num1 + "/" + num2 + "=");
+                    result = (float) num1 / num2;
+                }
+                resp = scan.nextFloat();
+
+                DecimalFormat df = new DecimalFormat("#.##");
+                df.setMinimumFractionDigits(0);
+                df.setMaximumFractionDigits(2);
+
+                respuestas = respuestas + "Ronda " + (ronda + 1) + ": ";
+                if (result == resp) {
+                    if (ronda <= 4) {
+                        respuestas = respuestas + resp + " es Correcto\n";
+                    } else {
+                        respuestas = respuestas + resp + " es Correcto";
+                    }
+                    puntos = puntos + 10;
+                } else {
+                    if (ronda <= 4) {
+                        respuestas = respuestas + df.format(resp) + " es Incorrecto, el resultado era: " + df.format(result) + "\n";
+                    } else {
+                        respuestas = respuestas + df.format(resp) + " es Incorrecto, el resultado era: " + df.format(result);
+                    }
+                }
+                ronda++;
+            }
+            System.out.println("=================================================== <[Resultados]> ===============================================");
+            System.out.print(respuestas);
+            System.out.print("Puntaje Final: " + (puntos / 5));
+            System.out.println("==================================================================================================================");
+        } catch (InputMismatchException e) {
+            System.out.println("=================================================== <[Error]> ====================================================");
+            System.err.println("Error, solo se admiten numeros");
+            System.out.println("==================================================================================================================");
+        }
     }
-    
 }
